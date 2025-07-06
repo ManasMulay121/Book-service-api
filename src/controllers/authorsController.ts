897a1f8dbf8adc6314f27cd6bd14711deb02248c
Manas <mpmulay971@gmail.com>
2025-07-06 10:49:23 +0530

    added function to update the author

--- src/controllers/authorsController.ts
+++ src/controllers/authorsController.ts
@@ -4,6 +4,9 @@ import pool from '../databasepg';
 export const getAuthors = async(req : Request, res : Response) => {
     try {
         const authors = await pool.query(`SELECT * FROM author`);
+        if(authors.rowCount === 0){
+            res.status(404).json({message : "Authors do not exist yet, add them first"});
+        }
         res.json(authors.rows);
     } catch (error) {
         console.log(error);
@@ -13,8 +16,8 @@ export const getAuthors = async(req : Request, res : Response) => {
 export const getAuthorById = async(req : Request, res : Response) => {
     try {
         const { id } = req.params;
-        const author = await pool.query(`SELECT * FROM author WHERE book_id = $1`, [id]);
-        if(author.rows.length === 0){
+        const author = await pool.query(`SELECT * FROM author WHERE author_id = $1`, [id]);
+        if(author.rowCount === 0){
             res.status(404).json({ message : "Author not found"});
         }
         res.json(author.rows[0]);
@@ -26,73 +29,29 @@ export const getAuthorById = async(req : Request, res : Response) => {
 export const createAuthor = async(req : Request, res : Response) => {
     try {
         const {name, bio} = req.body;
-        const addAuthor = await pool.query("INSERT INTO author(name, bio) VALUES ($1, $2) RETURNING *", [name, bio])
-        res.json(addAuthor.rows[0]);
+        const addAuthor = await pool.query("INSERT INTO author(name, bio) VALUES ($1, $2) RETURNING *", [name, bio]);
+        res.status(201).json({message : "new Author successfully added" ,author : addAuthor.rows[0]});
     } catch (error) {
         console.log(error);
+        res.status(500).json({message : "Could not add author into database"});
     }
 }
 
-
-
-// // const AUTHORS_FILE = '../data/author.json';
-
-// // Initialize next ID based on existing authors
-// let nextAuthorId = 1;
-// const AUTHORS_FILE = path.join(__dirname, '../data/author.json');
-// const existingAuthors: Author[] = readJSON(AUTHORS_FILE);
-// if (existingAuthors.length > 0) {
-//     const lastAuthor : Author = existingAuthors[existingAuthors.length - 1];
-//     nextAuthorId = lastAuthor._id + 1;
-// }
-
-// export const getAuthors = (req: Request, res: Response): void => {
-//     const authors = readJSON(AUTHORS_FILE);
-//     res.json(authors);
-// };
-
-// export const getAuthorById = (req: Request, res: Response): void => {
-//     const authors: Author[] = readJSON(AUTHORS_FILE);
-//     const author = authors.find(a => a._id === Number(req.params.id));
-//     if (!author) {
-//         res.status(404).json({ message: 'Author not found' });
-//         return;
-//     }
-//     res.json(author);
-// };
-
-// export const createAuthor = (req: Request, res: Response): void => {
-//     const authors: Author[] = readJSON(AUTHORS_FILE);
-//     const newAuthor: Author = {
-//         _id: nextAuthorId++,
-//         name: req.body.name,
-//         bio: req.body.bio || ''
-//     };
-//     authors.push(newAuthor);
-//     writeJSON(AUTHORS_FILE, authors);
-//     res.status(201).json(newAuthor);
-// };
-
-// export const updateAuthor = (req: Request, res: Response): void => {
-//     const authors: Author[] = readJSON(AUTHORS_FILE);
-//     const index = authors.findIndex(a => a._id === Number(req.params.id));
-//     if (index === -1) {
-//         res.status(404).json({ message: 'Author not found' });
-//         return;
-//     }
-//     authors[index] = { ...authors[index], ...req.body };
-//     writeJSON(AUTHORS_FILE, authors);
-//     res.json(authors[index]);
-// };
-
-// export const deleteAuthor = (req: Request, res: Response): void => {
-//     let authors: Author[] = readJSON(AUTHORS_FILE);
-//     const index = authors.findIndex(a => a._id === Number(req.params.id));
-//     if (index === -1) {
-//         res.status(404).json({ message: 'Author not found' });
-//         return;
-//     }
-//     const deleted = authors.splice(index, 1);
-//     writeJSON(AUTHORS_FILE, authors);
-//     res.json(deleted[0]);
-// };
+export const updateAuthor = async(req : Request, res : Response) => {
+    try {
+        const {id} = req.params;
+        const {name, bio} = req.body;
+        const updatedAuthor = await pool.query("UPDATE author SET name = $1, bio = $2 WHERE author_id = $3", [name, bio, id]);
+        if(updatedAuthor.rowCount === 0){
+            res.status(404).json({message : "Author not found, enter correct author ID"});
+            return;
+        }
+        res.status(201).json({
+            message: "Author details updated successfully", 
+            author : updatedAuthor.rows[0]
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Could not update Author, Server error"});
+    }
+}
