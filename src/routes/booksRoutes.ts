3ffd7f92f3ed74ac50845bcccb2a2203e539f4e7
Manas <mpmulay971@gmail.com>
2025-07-06 10:53:25 +0530

    modified booksRoutes to include update and delete books

--- src/routes/booksRoutes.ts
+++ src/routes/booksRoutes.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import {getBooks, getBookById, createBook} from  '../controllers/booksController'
-// import {updateBook, createBook, deleteBook} from '../controllers/booksController'
+import {getBooks, getBookById, createBook, updateBook, deleteBook} from  '../controllers/booksController'
 const router = express.Router();
 router.get('/', getBooks);
 router.get('/:id', getBookById);
 router.post('/', createBook);
-// router.put('/:id', updateBook);
-// router.delete('/:id', deleteBook);
+router.put('/:id', updateBook);
+router.delete('/:id', deleteBook);
 
 export default router;
