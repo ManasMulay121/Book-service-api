ab1d410191d9f81b93dc6a408d133dd4a7b508b5
Manas <mpmulay971@gmail.com>
2025-07-06 10:52:53 +0530

    added update and delete method to books

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express';
 import pool from '../databasepg';
+import { error } from 'console';
 
 export const getBooks = async(req : Request, res : Response) => {
     try {
-        const books = await pool.query(`SELECT * FROM book`);
-        res.json(books.rows);
+        const books = await pool.query(`SELECT 
+                                            b.book_id, 
+                                            b.title, 
+                                            b.published_year, 
+                                            json_agg(json_build_object('author_id', a.author_id, 
+                                                        'name', a.name,
+                                                        'bio', a.bio)) 	AS authors
+                                        FROM book b
+                                        JOIN book_author ba on ba.book_id = b.book_id
+                                        JOIN author a on a.author_id = ba.author_id
+                                        GROUP BY b.book_id, b.title, b.published_year;`);
+        res.status(200).json(books.rows);
     } catch (error) {
         console.log(error);
     }
@@ -13,82 +24,106 @@ export const getBooks = async(req : Request, res : Response) => {
 export const getBookById = async(req : Request, res : Response) => {
     try {
         const { id } = req.params;
-        const book = await pool.query(`SELECT * FROM book WHERE book_id = $1`, [id]);
+        const book = await pool.query(`SELECT 
+                                            b.book_id, 
+                                            b.title, 
+                                            b.published_year, 
+                                            json_agg(json_build_object('author_id', a.author_id, 
+                                                        'name', a.name,
+                                                        'bio', a.bio)) 	AS authors
+                                        FROM book b
+                                        JOIN book_author ba on ba.book_id = $1
+                                        JOIN author a on a.author_id = ba.author_id
+                                        WHERE b.book_id = $1
+                                        GROUP BY b.book_id, b.title, b.published_year;`, [id]);
         if(book.rows.length === 0){
-            res.status(404).json({ message : "Book not found"});
+            res.status(404).json({ error : "Book not found"});
+            return;
         }
-        res.json(book.rows[0]);
+        res.status(200).json(book.rows[0]);
     } catch (error) {
         console.log(error);
 
     }
 }
 export const createBook = async(req : Request, res : Response) => {
+    const client = await pool.connect();
     try {
+        
         const {title} = req.body;
-        const {published_year} = req.body;
-        const addBook = await pool.query("INSERT INTO book(title, published_year) VALUES ($1, $2) RETURNING *", [title, published_year])
-        res.json(addBook.rows[0]);
+        const {authorIds, published_year} = req.body;
+        await client.query("BEGIN");
+        const addBook = await client.query("INSERT INTO book(title, published_year) VALUES ($1, $2) RETURNING *", [title, published_year])
+        const bookId = addBook.rows[0].book_id;
+
+        for(const author of authorIds){
+            const checkAuthor = await client.query("SELECT * FROM author WHERE author_id = $1", [author]);
+            if(checkAuthor.rows.length === 0) {
+                await client.query("ROLLBACK");
+                res.status(404).json({message : `Author with id ${author} not found, create book again`});
+                return;
+            }
+            await client.query("INSERT INTO book_author(book_id, author_id) VALUES ($1, $2)", [bookId, author]);
+        }
+        await client.query("COMMIT");
+        res.status(201).json({ message : "Book created successfully" , book : addBook.rows[0]});
     } catch (error) {
         console.log(error);
+        await client.query("ROLLBACK");
+        res.status(500).json({error : "Could not create book"});
+    } finally {
+        client.release();
     }
 }
 
+export const updateBook =  async(req : Request, res : Response) => {
+    const client = await pool.connect();
+    try {
+        const {title, published_year, authorIds} = req.body;
+        const {id} = req.params
+        await client.query("BEGIN");
+        const updatedbook = await client.query("UPDATE book SET title = $1, published_year = $2 WHERE book_id = $3", [title, published_year, id]);
+        if(updatedbook.rowCount === 0){
+            await client.query("ROLLBACK");
+            res.status(404).json({message : "Book not found"});
+            return;
+        }
+        for (const author of authorIds){
+            const checkAuthor = await client.query("SELECT * FROM author WHERE author_id = $1", [author]);
+            if(checkAuthor.rows.length === 0){
+                await client.query("ROLLBACK");
+                res.status(404).json({error : `Author not found id : ${author}, please update again with correct author ids`});
+                return;
+            }
+            await client.query("INSERT INTO book_author (book_id, author_id) VALUES($1, $2) ON CONFLICT DO NOTHING", [id, author])
+        }
+        await client.query("COMMIT");
+        res.status(200).json({
+            message : "Book updated successfully!",
+            book : updatedbook.rows[0]
+        })
+    
+    } catch (error) {
+        await client.query("ROLLBACK");
+        console.log(error);
+        res.status(500).json({error : "Server error could not update book!"});
+    } finally{
+        client.release();
+    }
+}
 
-// const BOOKS_FILE = path.join(__dirname, '../data/books.json');
-
-// let nextBookId = 1;
-// const existingBooks : Book[] = readJSON(BOOKS_FILE);
-// if(existingBooks.length > 0){
-//     const lastBook : Book = existingBooks[existingBooks.length - 1];
-//     nextBookId = lastBook._id + 1;
-// }
-
-// export const getBooks = (req : Request, res : Response) : void => {
-//     const books = readJSON(BOOKS_FILE);
-//     res.json(books);
-// }
-// export const getBookById = (req : Request, res : Response) : void => {
-//     const books : Book[] = readJSON(BOOKS_FILE);
-//     const book = books.find(b => b._id === Number(req.params.id))
-//     if(!book){
-//         res.status(404).json({message : 'Book not found'});
-//         return;
-//     }
-//     res.json(book);
-// }
-// export const createBook = (req : Request, res : Response) : void => {
-//     const books : Book[] = readJSON(BOOKS_FILE);
-//     const newBook : Book = {
-//         _id : nextBookId++,
-//         title: req.body.title,
-//         publishedYear : req.body.publishedYear,
-//         authorIds : req.body.authorIds   
-//     };
-//     books.push(newBook);
-//     writeJSON(BOOKS_FILE, books);
-//     res.status(201).json(newBook);
-// } 
-// export const updateBook = (req : Request, res : Response) : void => {
-//     const books : Book[] = readJSON(BOOKS_FILE);
-//     const index = books.findIndex(b => b._id === Number(req.params.id));
-//     if(index === -1){
-//         res.status(404).json({message : 'Book not found'});
-//         return;
-//     }
-//     books[index] = { ...books[index], ...req.body};
-//     writeJSON(BOOKS_FILE, books);
-//     res.json(books[index]);
-// }
-// export const deleteBook = (req : Request, res : Response) : void => {
-//     let books : Book[] = readJSON(BOOKS_FILE);
-//     const index = books.findIndex(b => b._id === Number(req.params.id));
-//     if(index === -1){
-//         res.status(404).json({message : 'Book not found'});
-//         return;
-//     }
-//     const deleted = books.splice(index, 1);
-//     writeJSON(BOOKS_FILE, books);
-//     res.json(deleted[0]);
-// }
-
+export const deleteBook = async (req : Request, res : Response)  => {
+    try {
+        const {id} = req.params;
+        const book_auth_rel = await pool.query("DELETE FROM book_author WHERE book_id = $1", [id]);
+        const deletedBook = await pool.query("DELETE FROM book WHERE book_id = $1", [id]);
+        if(deletedBook.rowCount === 0){
+            res.status(404).json({message : "Book not found"});
+            return;
+        }
+        res.status(200).json({message : "Book successfully deleted", book : deletedBook.rows[0]});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error : "Could not delete book server error"})
+    }
+}
