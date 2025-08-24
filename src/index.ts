5773c2f26e56e9321ecf0a5e4653f726e7bb3207
Jayant Walvekar <jayant.walvekar@falconeer.health>
2025-08-24 11:48:34 +0530

    Added eslint and prettier rules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import authorsRoutes from './routes/authorsRoutes'
-import booksRoutes from './routes/booksRoutes'
+import express from "express";
+import dotenv from "dotenv";
+import authorsRoutes from "./routes/authorsRoutes";
+import booksRoutes from "./routes/booksRoutes";
 
 dotenv.config();
 
@@ -9,15 +9,19 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/books', booksRoutes);
-app.use('/authors', authorsRoutes);
+app.use("/books", booksRoutes);
+app.use("/authors", authorsRoutes);
 
+const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
-const PORT : number = parseInt(process.env.PORT as string, 10);
+if (isNaN(PORT)) {
+  console.error("Invalid PORT environment variable");
+  process.exit(1);
+}
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    console.log(`API endpoints:`);
-    console.log(`- Authors: http://localhost:${PORT}/authors`);
-    console.log(`- Books: http://localhost:${PORT}/books`);
-})
+  console.log(`Listening on port ${PORT}`);
+  console.log(`API endpoints:`);
+  console.log(`- Authors: http://localhost:${PORT}/authors`);
+  console.log(`- Books: http://localhost:${PORT}/books`);
+});
