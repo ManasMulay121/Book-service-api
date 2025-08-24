866e5de7c8af802c98e89287654318e4b7fb020b
Manas <mpmulay971@gmail.com>
2025-08-24 16:10:31 +0530

    refactored backend

--- server/database.sql
+++ server/database.sql
@@ -2,13 +2,17 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE author (
     author_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(100) NOT NULL,
-    bio TEXT
+    bio TEXT,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL
 );
 
 CREATE TABLE book (
     book_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     title VARCHAR(200) NOT NULL,
-    published_year INT
+    published_year INT,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL
 );
 
 CREATE TABLE book_author (
